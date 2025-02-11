98715ee727a2948e033bd68d4be9ca5e6042265f
Daniel Cosme <cosmedaniel8@gmail.com>
2025-02-11 18:07:58 -0500

    Update

--- vps/setup_fish_shell.sh
+++ vps/setup_fish_shell.sh
@@ -5,10 +5,11 @@ echo $MACHINES
 
 source $(realpath settings.sh)
 echo $VM_USER
+echo $VM_PASSWORD
 
 while read IP FQDN HOST; do
-    ssh -n $VM_USER@${HOST} "echo $VM_PASSWORD | chsh -s $(which fish)"
-    scp ./config/fish/fish_user_key_bindings.fish $VM_USER@${HOST}:~/.config/fish/functions/fish_user_key_bindings.fish
-    scp ./config/fish/config.fish $VM_USER@${HOST}:~/.config/fish/config.fish
+    ssh -n $VM_USER@${IP} "echo $VM_PASSWORD | chsh -s $(which fish)"
+    scp ./config/fish/fish_user_key_bindings.fish $VM_USER@${IP}:~/.config/fish/functions/fish_user_key_bindings.fish
+    scp ./config/fish/config.fish $VM_USER@${IP}:~/.config/fish/config.fish
 done < "$MACHINES"
 
