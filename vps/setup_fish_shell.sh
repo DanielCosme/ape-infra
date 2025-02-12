9db7cea2435d2b628a2690eebe0c95b60447f001
Daniel Cosme <cosmedaniel8@gmail.com>
2025-02-12 12:02:43 -0500

    Update fish shell setup

--- vps/setup_fish_shell.sh
+++ vps/setup_fish_shell.sh
@@ -9,7 +9,7 @@ echo $VM_PASSWORD
 
 while read IP FQDN HOST; do
     ssh -n $VM_USER@${IP} "echo $VM_PASSWORD | chsh -s $(which fish)"
-    scp ./config/fish/fish_user_key_bindings.fish $VM_USER@${IP}:~/.config/fish/functions/fish_user_key_bindings.fish
+    scp "./config/fish/fish_user_key_bindings.fish" $VM_USER@${IP}:~/.config/fish/functions/fish_user_key_bindings.fish
     scp ./config/fish/config.fish $VM_USER@${IP}:~/.config/fish/config.fish
 done < "$MACHINES"
 
